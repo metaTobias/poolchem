9bd9d1522eeb02ac9b1d3fa8dc700e03bf6a9984
Mettler <b.eiter@meta-edv.at>
2023-07-06 16:51:09 +0200

    added feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'poolchem';
+  title = 'poolchem my new site';
+  myarray = ["pool", "spielzeug"]
 }
