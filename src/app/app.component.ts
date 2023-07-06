db8ee82d63ee9b179d011c6562d33dfc846f6cca
metaTobias <t.eiter@meta-edv.at>
2023-07-06 18:28:13 +0200

    primeng setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FileUploadEvent} from "primeng/fileupload";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'poolchem';
+
+  onBasicUploadAuto($event: FileUploadEvent) {
+    alert("Basic Upload Auto")
+  }
+
+
+  onUpload($event: FileUploadEvent) {
+    alert("on Upload")
+  }
 }
